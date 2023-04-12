0c2dc3b0e2f67d861e9b90c674758c92a793c3a9
alszero <alszero05@gmail.com>
2023-04-12 09:49:01 +0900

    path.join()

--- 사진관리/fs.js
+++ 사진관리/fs.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
-
+const path = require('path');
 //__dirname: 현재 디렉토리의 절대경로(Node의 전역 변수)
-console.log(_dirname);
+console.log(__dirname);
 
 //해당 경로에 있는 파일 목록의 배열을 반환
 const files = fs.readdirSync(__dirname);
 
+// 문자열 합치는 것보다 join을 사용하는 것을 권장
+// join을 쓰는 이유: 경로방식을 운영체제에 맞춰서 처리해 줌
 //특정 파일의 전체(절대) 경로
-result = __dirname + '\\' + files[1];
+result = path.join(__dirname, files[1]);
 console.log(result);
 
 console.log(fs.statSync(result).isFile());
--- 사진관리/main.js
+++ 사진관리/main.js
@@ -6,9 +6,10 @@ const path = require('path');
 function getAllFiles(dirPath, arrOfFiles) {
     //dirPath에 있는 모든 파일목록의 배열(하위 경로는 불가)
     const files = fs.readdirSync(dirPath);
-
+    
     files.forEach(function(file) {
-      const destPath = dirPath+"\\"+file;
+      // const destPath = dirPath+"\\"+file;
+      const destPath = path.join(dirPath, file);
       //폴더(디렉토리)라면 해당 폴더에 있는 모든 파일목록들을 체크한다
         if(fs.statSync(dirPath + "\\" + file).isDirectory()) {
            getAllFiles(destPath,arrOfFiles);
