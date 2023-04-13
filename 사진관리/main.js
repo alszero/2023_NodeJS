a6bdf30d47ac3d837b8eb3b63ef096fd881d095d
alszero <alszero05@gmail.com>
2023-04-13 14:25:20 +0900

    getDuplicated() 디렉토리 점검

--- 사진관리/main.js
+++ 사진관리/main.js
@@ -1,25 +1,51 @@
-const { dir } = require('console');
 const fs = require('fs');
 const path = require('path');
 
-//dirPath에 있는 모든 파일목록의 배열(하위경로까지)
 function getAllFiles(dirPath, arrOfFiles) {
-    //dirPath에 있는 모든 파일목록의 배열(하위 경로는 불가)
+    //readdirSync는 해당 디렉토리의 파일 목록을 배열로 반환
+    // 즉, 모든 파일을 배열로 반환
     const files = fs.readdirSync(dirPath);
-    
+
     files.forEach(function(file) {
-      // const destPath = dirPath+"\\"+file;
-      const destPath = path.join(dirPath, file);
-      //폴더(디렉토리)라면 해당 폴더에 있는 모든 파일목록들을 체크한다
-        if(fs.statSync(dirPath + "\\" + file).isDirectory()) {
-           getAllFiles(destPath,arrOfFiles);
-        }else {
-          //파일이라면 파일목록에 추가
+        // const destPath = dirPath + "\\" + file;
+        const destPath = path.join(dirPath, file);
+
+        // 폴더(디렉토리)라면 해당 폴더에 있는 모든 파일목록들을 체크한다. 
+        if (fs.statSync(destPath).isDirectory()) {
+            getAllFiles(destPath, arrOfFiles);
+        } else {
+            // 파일이라면 파일 목록에 추가
             arrOfFiles.push(destPath);
         }
     });
     return arrOfFiles;
 }
 
-const files = getAllFiles(__dirname + "\\base", []);
-console.log(files.join('\n'));
+function getDuplicated(baseDir) {
+    //existsSync: 해당 경로에 파일이나 폴더가 존재하는지 확인
+    //파일이 존재하는지 유효성 검사 
+    if (!fs.existsSync(baseDir)) {
+        console.log("폴더가 존재하지 않음.");
+        return;
+    }
+
+    const duplicatedDir = path.join(baseDir, 'duplicated');
+    console.log(duplicatedDir);
+    // 폴더가 존재하지 않으면 폴더 생성
+    if (!fs.existsSync(duplicatedDir)) {
+        // mkdirSync: 폴더 생성
+        fs.mkdirSync(duplicatedDir);
+    }
+    // [] 없으면 에러 발생
+    // 그 이유는 getAllFiles 함수에서 arrOfFiles를 초기화 하지 않았기 때문
+    // base 폴더에 있는 모든 파일들의 목록(list)
+    const arrayOfFiles = getAllFiles(baseDir, []);
+    // 중복된 파일들(절대 경로) list
+    const duplicatedFiles = [];
+}
+
+// const files = getAllFiles(__dirname + "\\base", []);
+// const files = getAllFiles(path.join(__dirname, 'base'), []);
+// console.log(files.join('\n'));
+
+getDuplicated(path.join(__dirname, 'base'));
