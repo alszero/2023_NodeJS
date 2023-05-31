a866c08da4ebe45626803f98115d85778ca25969
alszero <alszero05@gmail.com>
2023-05-31 09:47:18 +0900

    express 기본

--- main.js
+++ main.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 const app = express();
 
+// 모든 경로에 요청
+app.get('*', function(req, res, next){
+  console.log('메렁렁');
+  next();
+})
+
+// 루트 경로에 get으로 요청하는 경우
 app.get('/', function(req, res, next) {
-  res.send('Hello World');
+  console.log('롱롱이지롱');
+  next();
 });
 
-app.listen(3333)
+app.get('/test/:name', function(req, res, next) {
+  console.log('path', req.path);
+  console.log('params', req.params);
+  console.log('query', req.query);
+  res.send('test중 콘솔을 보시오');
+})
+
+app.listen(3333);
